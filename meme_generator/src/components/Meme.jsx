b0d3268be971e916526c3dcfc46ca5491178198a
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-01-31 23:16:31 -0500

    practice control input

--- meme_generator/src/components/Meme.jsx
+++ meme_generator/src/components/Meme.jsx
@@ -20,16 +20,42 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <main>
       <div className='form'>
-        <input type='text' className='form--input' placeholder='top text' />
-        <input type='text' className='form--input' placeholder='bottom text' />
+        <input
+          type='text'
+          className='form--input'
+          placeholder='top text'
+          name='topText'
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          className='form--input'
+          placeholder='bottom text'
+          name='bottomText'
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button onClick={getMemeImg} className='form--button'>
           Generate Meme
         </button>
       </div>
       <img src={meme.randomImage} className='meme--image' />
+      <h2 className='meme--text top'>{meme.topText}</h2>
+      <h2 className='meme--text bottom'>{meme.bottomText}</h2>
     </main>
   );
 }
